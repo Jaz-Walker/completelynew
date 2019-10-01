dff81015fab46da15881ae9b68cfcc2c2abd1c66
Jaz Walker <playallday723@gmail.com>
2019-10-01 15:57:01 -0500

    Oh so I had to rush this since I had little time after my pycharm was fixed meaning that I did not commmit for every change.

---
+++ new.py
@@ -0,0 +1,8 @@
+print("say something")
+#notes are used to tell yourself what your code is for
+#You use print to display what you are doing or to show the solution to what you have done
+#I will be finding the multiplication of 5 and 7. I will do this by keeping the equation in parenthesis which will show what I want python to execute. I will use an astarisk to represent multiplication in the term that python can use
+print(5 * 7)
+#a floatpoint divides a real number and an integer into a decimal
+
+print(5 * 7 / 1)
